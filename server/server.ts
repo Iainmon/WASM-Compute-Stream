90e69c0e87e124e26b9206c30bed9a4e4dfe3b4a
Iain Moncrief <Iainmoncrief@gmail.com>
2019-10-15 11:29:24 -1000

    got server ready for haroku

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,17 @@
 import WebSocket from 'ws';
+const PORT = process.env.PORT || 3000;
 
 function randomRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 
-const wss = new WebSocket.Server({ port: 9001 });
+const wss = new WebSocket.Server({ port: PORT });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming() {
-        ws.send(randomRange(3, 46));
+        ws.send(randomRange(3, 48));
     });
 });
 
+console.log('WebSocket server started on port ', PORT);
